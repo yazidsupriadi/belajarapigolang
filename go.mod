f237442f8ee36cd6544b2232a82fc9794ede1698
yazidsupriadi <yazidsupriadi14@gmail.com>
2021-10-18 16:57:53 +0700

    crud api golang

--- go.mod
+++ go.mod
@@ -2,14 +2,20 @@ module belajarapigolang
 
 go 1.17
 
+require github.com/go-playground/validator v9.31.0+incompatible
+
+require github.com/go-sql-driver/mysql v1.6.0 // indirect
+
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.7.4 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -21,4 +27,6 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gorm.io/driver/mysql v1.1.2
+	gorm.io/gorm v1.21.16 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,101 @@
 package main
 
 import (
+	"belajarapigolang/book"
 	"belajarapigolang/handler"
+	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	router := gin.Default()
 
+	dsn := "root:@tcp(127.0.0.1:3306)/latihanapigolang?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("DB Connection error")
+	}
+	fmt.Println("Database Connected!")
+	db.AutoMigrate(&book.Book{})
+
+	//crud
+
+	//create or store data
+	/*
+		book := book.Book{}
+		book.Title = "Atomic Habits"
+		book.Price = 100000
+		book.Rating = 5
+		book.Description = "Buku self development"
+
+		err = db.Create(&book).Error
+		if err != nil {
+			fmt.Println("===========")
+
+			fmt.Println("There is an error occured!")
+			fmt.Println("===========")
+		}
+	*/
+
+	//single object
+	var book book.Book
+
+	//for list
+	//var books []book.Book
+
+	err = db.Debug().Where("id = ?", 1).First(&book).Error
+	if err != nil {
+		fmt.Println("===========")
+
+		fmt.Println("There is an error occured!")
+		fmt.Println("===========")
+	}
+
+	/*
+		fmt.Println("===========")
+
+		fmt.Println("Book Title : ", book.Title)
+		fmt.Println("Book Price %w : ", book)
+		fmt.Println("===========")
+	*/
+
+	/*
+		looping like foreach of list
+
+		for _, b := range books {
+			fmt.Println("Book Title : ", b.Title)
+			fmt.Println("Book Price %w : ", books)
+			fmt.Println("===========")
+
+		}
+	*/
+
+	/*
+
+		//updated data
+
+		book.Title = "Atomic Habits Revised Edition"
+		err = db.Save(&book).Error
+		if err != nil {
+			fmt.Println("===========")
+
+			fmt.Println("There is an error occured when updating record!")
+			fmt.Println("===========")
+		}
+	*/
+
+	err = db.Delete(&book).Error
+	if err != nil {
+		fmt.Println("===========")
+
+		fmt.Println("There is an error occured when updating record!")
+		fmt.Println("===========")
+	}
+	router := gin.Default()
 	//api route versioning
 	v1 := router.Group("/v1")
 
